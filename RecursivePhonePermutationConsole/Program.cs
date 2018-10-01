756ff3d1cf1be58adfa8f771a2601fe8eafc7f26
Derek Bathe <bathetrade@gmail.com>
2018-10-01 03:40:36 -0700

    Adding code.

---
+++ RecursivePhonePermutationConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using RecursivePhonePermute;
+using System;
+
+namespace RecursivePhonePermutationConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a program that takes in a series of digits from a phone keypad, and recursively permutes all possible letter combinations associated with those digits." +
+                "For example, the numbers '2' and '5' are associated with the letters 'abc' and 'jkl', respectively. Therefore, the permutations would be 'aj', 'ak', 'al', 'bj', 'bk', 'bl', 'cj', 'ck', 'cl'.");
+            Console.WriteLine();
+            Console.WriteLine("Enter a string of numbers between 2 and 9.");
+
+            var phoneNumbers = Console.ReadLine();
+
+            var phone = new Phone();
+            var firstPermutation = true;
+            phone.OnEachPermutation += (permutation) =>
+            {
+                if (!firstPermutation)
+                {
+                    Console.Write(", ");
+                }
+                Console.Write(permutation);
+                firstPermutation = false;
+            };
+
+            try
+            {
+                Console.WriteLine();
+                phone.PermutePhoneNumberLetterGroups(phoneNumbers);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+        }
+    }
+}
---
+++ RecursivePhonePermute/Phone.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace RecursivePhonePermute
+{
+    public class Phone
+    {
+        private static readonly string[] _letterGroups = new string[] { "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+
+        private int _numberOfLetterGroups;
+        private string _phoneNumbers;
+
+        private static bool OutOfRange(uint digit)
+        {
+            // 0 and 1 are not associated with letters on a typical phone keypad.
+            return digit < 2 || digit > 9;
+        }
+
+        private static void AssertDigitIsConvertibleToLetterGroup(uint digit)
+        {
+            if (OutOfRange(digit))
+            {
+                throw new ArgumentOutOfRangeException("Only the numbers 2-9 are associated with letters on a typical phone keypad.");
+            }
+        }
+
+        private static bool PhoneNumbersConvertibleToLetterGroups(string phoneNumbers)
+        {
+            if (String.IsNullOrEmpty(phoneNumbers))
+            {
+                return false;
+            }
+
+            // Make sure each character is a number that is in range.
+            foreach (char number in phoneNumbers)
+            {
+                if (!uint.TryParse(number.ToString(), out uint iNumber) || OutOfRange(iNumber))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /*  Terminology:
+        Assume the user enters "258". These numbers on a phone correspond to the 
+        letter groups "abc", "jkl", and "tuv", respectively.
+        "abc" has a letterGroupIndex of 0, "jkl" has a letterGroupIndex of 1, etc.
+        
+        Algorithm explanation:
+        The permutation algorithm runs like a nested loop, in the sense that the innermost loops
+        run more frequently than outer loops. Letter groups with a higher index are 
+        analogous to inner loops.
+        
+        For instance, in the example above with an input of "258", the output would be:
+        
+        ajt
+        aju
+        ajv
+        akt
+        aku
+        akv
+        
+        ...and so on. Notice that the last letter group "tuv" cycles more frequently
+        than the second, and the second cycles more frequently than the first.
+        */
+        private bool RecursivePermute(char[] permutation, int letterGroupIndex)
+        {
+            var finished = (letterGroupIndex == _numberOfLetterGroups);
+            if (finished)
+            {
+                return false;
+            }
+
+            var currentDigit = uint.Parse(_phoneNumbers[letterGroupIndex].ToString());
+            string currentLetterGroup = GetLetterGroup(currentDigit);
+
+            foreach(char letter in currentLetterGroup)
+            {
+                permutation[letterGroupIndex] = letter;
+                if (!RecursivePermute(permutation, letterGroupIndex + 1))
+                {
+                    var permutationString = new String(permutation);
+                    OnEachPermutation?.Invoke(permutationString);
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Converts digit on a phone's keypad to a letter group. For example, 2 would map to the letter group "abc".
+        /// </summary>
+        /// <param name="digit">A uint between 2 and 9. 0 and 1 do not map to any letters on a typical phone keypad. If the digit is invalid, an ArgumentOutOfRangeException is thrown.</param>
+        /// <returns>A letter group. For example, the digit 5 would return "jkl".</returns>
+        public static string GetLetterGroup(uint digit)
+        {
+            AssertDigitIsConvertibleToLetterGroup(digit);
+            return _letterGroups[digit - 2];
+        }
+
+        public void PermutePhoneNumberLetterGroups(string phoneNumbers)
+        {
+            if (!PhoneNumbersConvertibleToLetterGroups(phoneNumbers))
+            {
+                throw new ArgumentException($"Each character in {nameof(phoneNumbers)} must be a number between 2 and 9. Also, {nameof(phoneNumbers)} cannot be null or empty.");
+            }
+
+            _phoneNumbers = phoneNumbers;
+            _numberOfLetterGroups = _phoneNumbers.Length;
+            var permutation = new char[_numberOfLetterGroups];
+            RecursivePermute(permutation, 0);
+        }
+
+        public event Action<string> OnEachPermutation;
+    }
+}
